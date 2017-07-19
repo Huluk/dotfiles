21871b6b3d9af50780558482e73fbfb3a1c77a56
LarsHuluk <LarsHuluk@gmail.com>
2017-07-19 22:02:29 +0200

    Update vim

--- vim/update_plugins.rb
+++ vim/update_plugins.rb
@@ -13,6 +13,8 @@ git_bundles = [
   "git://github.com/godlygeek/tabular.git",
   "git://github.com/tpope/vim-markdown.git",
   "git://github.com/derekwyatt/vim-scala",
+  "git://github.com/lambdatoast/elm.vim",
+  "git://github.com/raichoo/purescript-vim",
   "git://github.com/tpope/vim-repeat.git",
   # "git://github.com/jimenezrick/vimerl.git",
   # "git://github.com/tsaleh/vim-matchit.git", # included in vim distr
