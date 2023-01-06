e422ff884fe0e4585b2048f0d68677fb5a01e9ad
Lars Hansen <larsthansen@google.com>
2023-01-06 11:33:49 +0100

    Remove old LSP configs and clean up

--- vim/lua/lsp_config.lua
+++ vim/lua/lsp_config.lua
@@ -1,31 +1,4 @@
--- Works for work setup (g:at_work) only
-
-local nvim_lsp = require('lspconfig')
-local configs = require('lspconfig.configs')
-local coq = require('coq')
-
-configs.ciderlsp = {
-  default_config = {
-    cmd = {'/google/bin/releases/cider/ciderlsp/ciderlsp', '--tooltag=nvim-lsp' , '--noforward_sync_responses'};
-    filetypes = {'c', 'cpp', 'java', 'kotlin', 'objc', 'proto', 'textproto', 'go', 'python', 'bzl'};
-    root_dir = nvim_lsp.util.root_pattern('BUILD');
-    settings = {};
-  };
-}
-
-local preview_location_callback = function(_, result)
-  if result == nil or vim.tbl_isempty(result) then
-    return nil
-  end
-  vim.lsp.util.preview_location(result[1])
-end
-
-peek = function(f)
-  local params = vim.lsp.util.make_position_params()
-  return vim.lsp.buf_request(0, f, params, preview_location_callback)
-end
-
-local custom_attach = function(client, bufnr)
+custom_attach = function(client, bufnr)
   print("LSP started.");
 
   local map = function(key, value)
@@ -96,16 +69,3 @@ local custom_attach = function(client, bufnr)
   end
   vim.api.nvim_command("augroup END")
 end
-
-nvim_lsp.ciderlsp.setup{ on_attach = custom_attach }
-
-nvim_lsp.ciderlsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = false,
-    signs = true,
-    update_in_insert = true,
-  }
-)
-
-nvim_lsp.ciderlsp.setup(coq.lsp_ensure_capabilities{ on_attach = custom_attach })
-vim.api.nvim_command('COQnow -s')
